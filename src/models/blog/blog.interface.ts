841df68a58987869be0ab4a8f5a3f3004f63fab3
alahirajeffrey <alahirajeffrey@gmail.com>
2022-05-30 09:37:21 -0700

    added variable

--- src/models/blog/blog.interface.ts
+++ src/models/blog/blog.interface.ts
@@ -1,6 +1,9 @@
+import { User } from "../users/user.interface"
+
 export interface BlogPost{
     id? : number
     blogTitle : string
     blogPost : string
     createdAt: Date
+    author: User
 }
