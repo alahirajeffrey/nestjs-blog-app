759f966728d8b28783146d9c10f1a57d0a37a879
alahirajeffrey <alahirajeffrey@gmail.com>
2022-05-31 13:15:47 -0700

    commit

--- src/models/blog/blog.interface.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../users/user.interface"
-
-export interface BlogPost{
-    id? : string
-    blogTitle : string
-    blogPost : string
-    createdAt: Date
-    author: User
-}
